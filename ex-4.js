63899a9a324e9b9a9e2d14f2f6d70fc311ed6ecd
Park <tanagrit.sornchaiyagorn@gmail.com>
2023-11-30 13:48:47 +0700

    Add : Add condition(if, else if, switch) stament to ex1-7 files

--- ex-4.js
+++ ex-4.js
@@ -1,5 +1,21 @@
 //Exercise 4
 // Switch Statement
 let lightBulbStatus = "On";
+let lightMessage;
 
-//Start coding here
+switch (lightBulbStatus) {
+  case "On":
+    lightMessage = "Light blub is On.";
+    break;
+  case "Off":
+    lightMessage = "Light blub is Off.";
+    break;
+  case "Broken":
+    lightMessage = "Light blub is Broken.";
+    break;
+  default:
+    lightMessage = "Please choose the correct input (On/Off/Broken)";
+    break;
+}
+
+console.log(lightMessage);
